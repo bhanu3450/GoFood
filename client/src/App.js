943ae4aa8ad4683a4abc4f57b1135b9bf12b1e4e
bhanu <bhanu3450@gmail.com>
2024-02-26 23:44:40 +0530

    created navbar and home components

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,19 @@
 
 import './App.css';
-
+import Home from './screens/Home';
+import {
+  BrowserRouter as Router,
+  Routes, Route, Link
+} from "react-router-dom";
 function App() {
   return (
-    <>
-    
-    </>
+    <Router>
+    <div> 
+      <Routes>
+        <Route exact path = "/" element = {<Home/>} />
+        </Routes> 
+      </div>
+    </Router>
     
   );
 }
